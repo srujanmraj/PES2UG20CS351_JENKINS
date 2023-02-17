aac5d3ee9f19507edfbae2cef06dbd1279ac0808
Srujan M Raj <79003814+srujanmraj@users.noreply.github.com>
2023-02-17 10:19:19 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "Srujan M Raj: PES2UG20CS351\N";
   return 0;
 }
